9a943c6cc2b8e6f81cfbf628f87817b7882efe74
Nancy (via Fern) <nancy@jamello.com>
2026-04-27 09:37:12 -0700

    Refine donate section text

--- src/App.tsx
+++ src/App.tsx
@@ -502,7 +502,7 @@ const App = () => {
               mb="24px"
             >
               <em>
-                If you enjoy Nancy&apos;s yoga instruction, please consider supporting by donating. Thank you.
+                Enjoy Nancy&apos;s yoga instruction? Please consider supporting by donating. Thank you
               </em>
             </Text>
             <Button
